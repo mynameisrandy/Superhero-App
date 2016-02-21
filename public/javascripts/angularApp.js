90e5d1c418da7f11786b8b571295cdf923f761da
Randy Phalla <randyphalla@gmail.com>
2016-02-21 18:30:02 -0500

    ngAnimate was added

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,6 +1,6 @@
 
 
-var superheroApp = angular.module('superheroApp', ['ui.router']);
+var superheroApp = angular.module('superheroApp', ['ui.router', 'ngAnimate']);
 
 //-------------------CONFIG---------------------//
 
@@ -31,26 +31,31 @@ superheroApp.config(['$stateProvider', '$urlRouterProvider', function($stateProv
 //-------------------CONTROLLERS---------------------//
 
 
+
 // List
 superheroApp.controller('ListController', ['$scope', '$http', function($scope, $http) {
-
 	$http.get('/characters').then(function(characters) {
 		console.log(characters.data);
 		$scope.characters = characters.data;
+		// ngAnimate 
+		$scope.pageClass = 'list';
 	});
 
 }]);
-
 
 // Details
 superheroApp.controller('detailsController', ['$scope', '$http','$stateParams', function($scope, $http, $stateParams) {
 	var ID = $stateParams.characterId;
 	$http.get('/characters/'+ID).then(function(characters) {
 		console.log(characters.data.name);
+
 		$scope.alias = characters.data.alias;
 		$scope.city = characters.data.city;
 		$scope.name = characters.data.name;
 		$scope.image = characters.data.image;
 		$scope.affiliation = characters.data.affiliation;
+		
+		// ngAnimate 
+		$scope.pageClass = 'details';
 	});
 }]);
